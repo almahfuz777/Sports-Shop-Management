6e293658cc9f924290717aadeeb3572643079dc3
MotasimAbid <106864299+MotasimAbid@users.noreply.github.com>
2022-12-24 00:25:48 +0600

    2nd push

--- src/main/java/com/example/sportsshopmanagement/Main.java
+++ src/main/java/com/example/sportsshopmanagement/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,6 +20,7 @@ public class Main extends Application {
         try{
 
             Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+
             Scene scene = new Scene(root);
             stage.setTitle("ByteMeDirtyBites");
             stage.setScene(scene);
